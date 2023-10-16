a75eb0ab8843e2a1e81d79aa371a1fc11d47ee25
Maxime Riaud PORTABLE <maxime.riaud@gmail.com>
2023-10-16 19:07:57 +0200

    client connect first draft

--- src/PTSL/mod.rs
+++ src/PTSL/mod.rs
@@ -1,12 +1,15 @@
 pub mod SDK {
     tonic::include_proto!("ptsl");
 }
+
 use tonic::transport::Channel;
 use SDK::ptsl_client::PtslClient;
 use SDK::{Request, RequestHeader, Response};
 
+use crate::PTSL::SDK::{ResponseHeader, TaskStatus};
+
 use self::SDK::{CommandId, RegisterConnectionRequestBody, RegisterConnectionResponseBody};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug)]
 pub enum Error {
@@ -14,6 +17,8 @@ pub enum Error {
     SerdeErr(serde_json::Error),
     NetErr(tonic::Status),
     TransportErr(tonic::transport::Error),
+    PTStatus(TaskStatus),
+    PTError,
 }
 
 pub struct Client {
@@ -37,49 +42,73 @@ impl Client {
         };
 
         let request = tonic::Request::new(Request {
-            header: None,
+            header: Some(RequestHeader {
+                task_id: String::new(),
+                command: CommandId::RegisterConnection.into(),
+                version: 3,
+                session_id: String::new(),
+            }),
             request_body_json: serde_json::to_string(&body).map_err(|err| Error::SerdeErr(err))?,
         });
+
+        println!("Send request {:?}", request);
         let response = client
             .send_grpc_request(request)
             .await
             .map_err(|status| Error::NetErr(status))?;
 
-        let body_response = response.into_inner().response_body_json;
+        let body = response.into_inner();
 
+        match body.header {
+            Some(ResponseHeader { status, .. }) => match status {
+                3 => {} // TaskStatus::
+                _ => return Err(Error::PTError),
+            },
+            _ => return Err(Error::PTError),
+        }
+
+        // println!("meta: {:?}, body: {:?}, ext: {:?}", meta, body, ext);
         let response: RegisterConnectionResponseBody =
-            serde_json::from_str(&body_response).map_err(|err| Error::SerdeErr(err))?;
+            serde_json::from_str(&body.response_body_json).map_err(|err| Error::SerdeErr(err))?;
         Ok(Self {
             client,
             session_id: response.session_id,
         })
     }
 
-    pub async fn request<T: Serialize>(
+    pub async fn request<Req: Serialize, Resp>(
         &mut self,
-        command: &str,
-        payload: T,
-    ) -> Result<tonic::Response<Response>, Error> {
-        match CommandId::from_str_name(command) {
-            Some(command_id) => {
-                let header = RequestHeader {
-                    command: command_id.into(),
-                    task_id: String::new(),
-                    version: 3,
-                    session_id: self.session_id.clone(),
-                };
+        command_id: CommandId,
+        payload: Req,
+    ) -> Result<Resp, Error> {
+        let header = RequestHeader {
+            command: command_id.into(),
+            task_id: String::new(),
+            version: 3,
+            session_id: self.session_id.clone(),
+        };
 
-                let request_body_json =
-                    serde_json::to_string(&payload).map_err(|err| Error::SerdeErr(err))?;
-                self.client
-                    .send_grpc_request(Request {
-                        header: Some(header),
-                        request_body_json,
-                    })
-                    .await
-                    .map_err(|status| Error::NetErr(status))
+        let request_body_json =
+            serde_json::to_string(&payload).map_err(|err| Error::SerdeErr(err))?;
+
+        println!("Send request {:?}", request_body_json);
+        let resp = self
+            .client
+            .send_grpc_request(Request {
+                header: Some(header),
+                request_body_json,
+            })
+            .await
+            .map_err(|status| Error::NetErr(status))?
+            .into_inner();
+        if let Some(header) = resp.header {
+            match header.status {
+                3 => serde_json::from_str(&resp.response_body_json)
+                    .map_err(|err| Error::SerdeErr(err))?,
+                _ => serde_json::from_str(&resp.response_error_json)
+                    .map_err(|err| Error::SerdeErr(err))?,
             }
-            None => Err(Error::CommandNotFound),
         }
+        Err(Error::PTError)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
-use test_grpc::PTSL;
+use test_grpc::PTSL::{
+    self,
+    SDK::{
+        CommandId, CreateNewTracksRequestBody, CreateNewTracksResponseBody, TrackFormat,
+        TrackTimebase, TrackType,
+    },
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut _client = PTSL::Client::connect(
+    let mut client = PTSL::Client::connect(
         String::from("http://[::1]:31416"),
         String::from("CuetesCuetos.com"),
         String::from("kikikiki.com"),
     )
     .await
     .unwrap();
+
+    println!(
+        "{:?}",
+        client
+            .request::<CreateNewTracksResponseBody>(
+                CommandId::CreateNewTracks,
+                CreateNewTracksRequestBody {
+                    number_of_tracks: 1,
+                    track_name: String::from("test"),
+                    track_format: TrackFormat::TfStereo.into(),
+                    track_type: TrackType::TtAudio.into(),
+                    track_timebase: TrackTimebase::TtbSamples.into(),
+                },
+            )
+            .await
+    );
 
     // client.request(command, payload)
 
